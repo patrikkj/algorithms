46f2cc5ed6daf434ce917cf2b2ccf80e860dc70f
Patrik Kjærran <patrikkja@gmail.com>
2020-09-20 14:27:27 +0200

    Implement interactive k-means clustering.

--- clustering/clustering.py
+++ clustering/clustering.py
@@ -1,41 +1,74 @@
 import numpy as np
+import common.utils as utils
 
-X = np.random.rand(10, 2)
 
-def k_means(X, centroids=None, k=3):
+def k_means(X, centroids=None, k=3, norm=2):
+    """Implementation of K-means clustering.
+    
+    Args:
+        X (ndarray[m, n]):              input dataset
+        centroids ([type], optional):   initial centroids (defaults to random)
+        k (int, optional):              number of clusters
+    
+    Returns:
+        centroids (ndarray[k, n]):      cluster centers
+        labels (ndarray[m]):            labels of each point
+    """
     # Initialize randomly if no centroids are provided
     if centroids is None:
         centroids = X[np.random.choice(X.shape[0], size=k, replace=False)]
+    k = centroids.shape[0]
 
     prev_centroids = None
+    i = 1
     while not np.array_equal(centroids, prev_centroids):
+        print('\n')
+        print('—'*50)
+        print(f"{'Iteration ' + str(i):^50}")
+        print('—'*50)
+        i += 1
         prev_centroids = np.copy(centroids)
 
         # Assign cluster membership to every point
-        distances = np.linalg.norm(X[:, np.newaxis, :] - centroids, axis=-1)
-        indices = np.argmin(distances, axis=1).reshape(-1, 1)
-        one_hot = (indices == np.arange(k))[..., np.newaxis]
-        one_hotb = np.repeat(one_hot, X.shape[-1], axis=-1)
-        print("onehot\n\n", one_hot.shape)
-        print("onehotb\n\n", one_hotb.shape)
+        distances = np.linalg.norm(X[:, np.newaxis, :] - centroids, axis=-1, ord=norm)
+        print("Distance to centroids")
+        print(distances)
+
+        labels = np.argmin(distances, axis=1)
+        print("\nCluster membership")
+        print(np.array2string(labels.reshape(-1, 1), formatter={'all': lambda e: f'C{e+1:<2}'}))
+
+        # Print clusters
+        print("\nNew clusters")
+        clusters = utils.groupby(enumerate(sorted(labels)), key=lambda tup: tup[1])
+        for cluster_index, points in clusters.items():
+            cluster_str = f"C{str(cluster_index+1) + ':':<3}"
+            members_str = '[' + ', '.join(f'P{p[0]+1}' for p in points) + ']'
+            print(cluster_str, members_str)
 
         # Move centroids
-        clusters = X[:, None, :] * one_hot
-        clusters[~one_hotb] = np.nan
-        centroids = np.nanmean(clusters, axis=0)
-        print(clusters)
+        centroids = np.array([np.mean(X[labels==i], axis=0) for i in range(k)])
+        print("\nNew centroids")
         print(centroids)
+    return centroids, labels
 
-        clusters = [X[indices==i] for i in range(centroids.shape[0])]
-        centroids = np.array([np.average(points, 0) for points in clusters])
-    return centroids
 
-k_means(X)
-
-def k_means_fully_vectorized(X, centroids=None, k=3):
+def k_means_vec(X, centroids=None, k=3):
+    """Fully vectorized implementation of K-means clustering.
+    
+    Args:
+        X (ndarray[m, n]):              input dataset
+        centroids ([type], optional):   initial centroids (defaults to random)
+        k (int, optional):              number of clusters
+    
+    Returns:
+        centroids (ndarray[k, n]):      cluster centers
+        labels (ndarray[m]):            labels of each point
+    """
     # Initialize randomly if no centroids are provided
     if centroids is None:
         centroids = X[np.random.choice(X.shape[0], size=k, replace=False)]
+    k = centroids.shape[0]
 
     prev_centroids = None
     while not np.array_equal(centroids, prev_centroids):
@@ -43,13 +76,27 @@ def k_means_fully_vectorized(X, centroids=None, k=3):
 
         # Assign cluster membership to every point
         distances = np.linalg.norm(X[:, np.newaxis, :] - centroids, axis=-1)
-        indices = np.argmin(distances, axis=1).reshape(-1, 1)
-        one_hot = (indices == np.arange(k))[..., np.newaxis]
-        mask = np.repeat(one_hot, X.shape[-1], axis=-1)
+        labels = np.argmin(distances, axis=1)
+
+        # Create masks for vectorization
+        mask_2d = (labels == np.arange(k)[:, np.newaxis]).astype(float)
+        mask_2d[mask_2d == 0] = np.nan
+        mask_3d = np.broadcast_to(np.atleast_3d(mask_2d), (k, *X.shape))
 
         # Move centroids
-        clusters = np.ma.array(X)
-        clusters = X[:, None, :] * one_hot
-        clusters[~one_hotb] = np.nan
-        centroids = np.nanmean(clusters, axis=0)
-    return centroids
+        clusters = X * mask_3d
+        centroids = np.nanmean(clusters, axis=1)
+    return centroids, labels
+
+
+def labels_to_clusters(X, labels):
+    """Utility function for converting labels to clusters.
+    
+    Args:
+        X (ndarray[m, n]):              input dataset
+        labels (ndarray[m]):            labels of each point
+    
+    Returns:
+        clusters (list, ndarray[i, n]): list of clusters (i denotes the size of each cluster)
+    """
+    return [X[labels==i] for i in range(labels.max() + 1)]
